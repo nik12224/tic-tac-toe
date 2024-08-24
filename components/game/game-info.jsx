31f2ba6bd7e3fe21189ab282d320cb35ae33ec8c
nik12224 <0lezgin5@gmail.com>
2024-08-24 12:24:09 +0500

    рефакторит стили в tailwind

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,9 +1,8 @@
-import styles from './game.module.css'
 import { GameSymbol } from './game-symbol'
 
 export function GameInfo({ isWin, winnerSequence, currentStep, winnerSymbol }) {
 	return (
-		<div className={styles['game-info']}>
+		<div className="mb-2.5">
 			{isWin ? (
 				'Ничья'
 			) : (
--- components/game/game.js
+++ components/game/game.js
@@ -1,6 +1,5 @@
 import { GameCell } from './game-cell'
 import { GameInfo } from './game-info'
-import styles from './game.module.css'
 import { useGameState } from './use-game-state'
 
 export default function Game() {
@@ -15,15 +14,15 @@ export default function Game() {
 	} = useGameState()
 
 	return (
-		<div className={styles['game']}>
+		<div className="flex flex-col items-center w-40 mx-auto my-24 border border-black p-5">
 			<GameInfo isWin={isWin} winnerSymbol={winnerSymbol} currentStep={currentStep} />
-			<div className={styles['game-field']}>
+			<div className="grid pt-px pl-px grid-cols-[repeat(3,_30px)] grid-rows-game-field">
 				{cells.map((symbol, index) => {
 					const isWinner = winnerSequence?.includes(index)
 					return <GameCell key={index} symbol={symbol} onClick={() => handleCellClick(index)} />
 				})}
 			</div>
-			<div className={styles['game-clean']}>
+			<div className="cursor-pointer mt-2.5 bg-transparent border border-gray-400 py-1 px-3 rounded">
 				<button onClick={handleCleanClick}>Сбросить</button>
 			</div>
 		</div>
